a93dd515eb18ec3fabb05e659a77523d936b575b
huangran <rhuang@kingdomai.com>
2021-11-18 14:01:46 +0800

    u

--- src/apply/test/mongo_test/test_monogo.py
+++ src/apply/test/mongo_test/test_monogo.py
@@ -8,26 +8,27 @@ class Es(unittest.TestCase):
         self.client = pymongo.MongoClient('mongodb://localhost:27017/')
 
     def test_main(self):
-        db = self.client.mydemo
-        collection = db["students"]
+        db = self.client["test"]
+        student_table = db["student"]
         student = {'id': '20170101', 'name': '小明', 'age': 22, 'gender': '男性'}
         # 插入 insert_one insert_many
-        result = collection.insert(student)
+        result = student_table.insert(student)
 
-        query_filter = {'name': 'Mike'}
-        student = collection.find_one(query_filter)
+        query_filter = {'name': '小明'}
+        student = student_table.find_one(query_filter)
         student['age'] = 25
         # 更新 update_one update_many
-        result = collection.update(query_filter, {'$set': student})
+        result = student_table.update(query_filter, {'$set': student})
 
         # 查找
-        results = db.collection.find()
-        results = collection.find({"name": {"$regex": "^M.*"}})
-        results = collection.find().sort('name', pymongo.ASCENDING).skip(1).limit(1)
+        results = list(student_table.find())
+        results = list(student_table.find({"name": {"$regex": "^小.*"}}))
+        results = list(student_table.find().sort('name', pymongo.ASCENDING).skip(0).limit(10))
+
         # 删除
-        result = collection.remove({'name': 'Jordan'})
-        result = collection.delete_one({'name': 'Kevin'})
-        result = collection.delete_many({'name': 'Kevin'})
+        result = student_table.remove({'name': 'Jordan'})
+        result = student_table.delete_one({'name': 'Kevin'})
+        result = student_table.delete_many({'name': 'Kevin'})
 
     def tear_down(self):
         self.es.close()
