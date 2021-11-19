93dc2e6124cad2cde4f0faf12e496f68d590598f
huangran <rhuang@kingdomai.com>
2021-11-19 17:50:56 +0800

    u

--- src/util/tree_util.py
+++ src/util/tree_util.py
@@ -8,20 +8,19 @@ from copy import deepcopy
 
 def tree_to_list(data_list):
     ret = []
-    if not data_list:
-        return ret
-
     for item in data_list:
-        ret.extend(tree_to_list(_release_children(item)))
+        if item.get("children") and isinstance(item.get("children"), list):
+            ret.extend(tree_to_list(_release_children(item)))
+        else:
+            return data_list
     return ret
 
 
 def _release_children(dic):
-    assert isinstance(dic.get("children"), list), "数据格式错误,缺少children字段"
     child = dic.pop("children")
     # child = dic.get("children")
     if child:
-        repeat = set(dic).union(set(child[0]))
+        repeat = set(dic) & (set(child[0]))
         assert not repeat, "命名重复".format(repeat)
         ret = []
         for item in child:
