807559e7c96aab785c7d6aaa455b9d4688297842
wg <1134614268@qq.com>
2021-12-19 20:12:09 +0700

    u

--- src/apply/数据库/test_date.py
+++ src/apply/数据库/test_date.py
@@ -6,7 +6,7 @@
 from datetime import datetime
 from unittest import TestCase
 
-from sqlalchemy import Column, create_engine, DateTime, Integer, TIMESTAMP
+from sqlalchemy import Column, create_engine, DateTime, Integer, TIMESTAMP, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.functions import func
@@ -25,11 +25,12 @@ class BaseTable(Base):
 
 class TestDate(BaseTable):
     __tablename__ = "test_date_t"
+    date = Column(DateTime)
+    time = Column(DateTime)
     date_time = Column(DateTime)
-    date_time_auto = Column(DateTime)
-    date_time_str = Column(DateTime)
-    time_stamp = Column(TIMESTAMP, server_default=func.current_timestamp(), server_onupdate=func.current_timestamp(),
-                        onupdate=func.current_timestamp())
+    timestamp = Column(TIMESTAMP, server_default=func.current_timestamp(), server_onupdate=func.current_timestamp(),
+                       onupdate=func.current_timestamp())
+    date_time_str = Column(String)
 
 
 def exec_sql(sql):
@@ -44,20 +45,16 @@ def exec_sql(sql):
 
 class TestMysql(TestCase):
     def setUp(self):
-        sqlserver_url = 'mysql+mysqlconnector://{}:{}@{}/{}'.format(
-            'wg', "123456", "ggok.top", 'test')
+        sqlserver_url = 'mysql+mysqlconnector://{}:{}@{}/{}?time_zone={}'.format(
+            'root', "123456", "127.0.0.1", 'test', "%2B10:00")
         engine = create_engine(sqlserver_url, echo=True)
         # Base.metadata.drop_all(engine)
         # Base.metadata.create_all(engine)
         global session;
         session = sessionmaker(bind=engine)()
-        exec_sql("show VARIABLES like '%time_zone%'; ")
-
-        sql2 = "set time_zone = '+03:00';"
-        exec_sql("show VARIABLES like '%time_zone%'; ")
-        exec_sql("set time_zone = '+03:00';")
-        sql3 = "flush privileges;"
-        exec_sql(sql3)
+        # exec_sql("show VARIABLES like '%time_zone%'; ")
+        # exec_sql("set time_zone = '+03:00';")
+        # exec_sql("show VARIABLES like '%time_zone%'; ")
 
     def test_a(self):
         # 服务器时区 GTM+8 本机 GTM+8 url GTM+8
@@ -66,15 +63,18 @@ class TestMysql(TestCase):
 
         # mysql 默认 SYSTEM
 
-        new_time = datetime(2021, 12, 1, 12, 00, 00)
+        new_time = datetime(2021, 1, 1, 12, 00, 00)
         session.add(
-            TestDate(create_time=datetime.now(),
-                     update_time="2021-12-01 12:00:00",
-                     date_time=new_time, )
+            TestDate(
+                date=datetime.now(),
+                time=datetime.now(),
+                datetime=datetime.now(),
+                date_time_str="2000-01-01 00:00:00",
+            )
         )
         session.commit()
-        sql2 = "set time_zone = '+12:00';"
-        exec_sql(sql2)
+        # sql2 = "set time_zone = '+12:00';"
+        # exec_sql(sql2)
 
         vos = session.query(TestDate).all()
         print()
