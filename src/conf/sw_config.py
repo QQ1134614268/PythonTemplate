48545525e9bca723b99450bcbb49b098c66f2044
huangran <rhuang@kingdomai.com>
2022-04-01 18:10:16 +0800

    u

--- src/conf/sw_config.py
+++ src/conf/sw_config.py
@@ -6,6 +6,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SHENWAN_URL = 'mysql+pymysql://{}:{}@{}:{}/{}'.format('root', "6HOoIAqc22uw7gc2", "124.70.47.167", "20010", 'pledge_risk_01')
+SHENWAN_URL = 'mysql+pymysql://{}:{}@{}:{}/{}'.format('root', "6HOoIAqc22uw7gc2", "124.70.47.167", "20011", 'pledge_risk_01')
 shenwan_engine = create_engine(SHENWAN_URL, echo=True)
 shenwan_session = sessionmaker(bind=shenwan_engine)()
