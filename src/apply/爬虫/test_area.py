c3cee479383d3dffc38c10b02fce33c3f428eaf7
huangran <huangran@subinszkj.com>
2022-07-15 15:23:23 +0800

    u

---
+++ src/apply/爬虫/test_area.py
@@ -0,0 +1,68 @@
+import time
+from unittest import TestCase
+
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import declarative_base
+
+from conf.config import localhost_test_session, localhost_test_engine
+
+Base = declarative_base()
+
+
+class Area(Base):
+    __tablename__ = 'area'
+    id = Column(INTEGER, primary_key=True, comment="id")
+    area_name = Column(String(64), index=True, comment="名称")
+    parent_id = Column(INTEGER, index=True, comment="父级id")
+
+
+class Test(TestCase):
+    def test_pre(self):
+        ...
+        # Base.metadata.drop_all(localhost_test_engine)
+        Base.metadata.create_all(localhost_test_engine)
+
+    def test_main(self):
+        base_url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2021/'
+
+        trs = self.get_response(base_url, 'provincetr')
+        for tr in trs:  # 循环每一行
+            datas = []
+            for td in tr:  # 循环每个省
+                province_name = td.a.get_text()
+                province_url = base_url + td.a.get('href')
+                province_vo = Area(area_name=province_name)
+                localhost_test_session.add(province_vo)
+                localhost_test_session.commit()
+                trs = self.get_response(province_url, None)
+                for tr in trs[1:]:  # 循环每个市
+                    city_code = tr.find_all('td')[0].string
+                    city_name = tr.find_all('td')[1].string
+                    city_url = base_url + tr.find_all('td')[1].a.get('href')
+                    trs = self.get_response(city_url, None)
+                    city_vo = Area(area_name=str(city_name), parent_id=province_vo.id)
+                    localhost_test_session.add(city_vo)
+                    localhost_test_session.commit()
+                    for tr in trs[1:]:  # 循环每个区
+                        county_code = tr.find_all('td')[0].string
+                        county_name = tr.find_all('td')[1].string
+                        data = [province_name, city_code, city_name, county_code, county_name]
+                        county_vo = Area(area_name=str(county_name), parent_id=city_vo.id)
+                        localhost_test_session.add(county_vo)
+                        localhost_test_session.commit()
+                        datas.append(data)
+                    time.sleep(1)
+
+    def get_response(self, url, attr):
+        response = requests.get(url)
+        response.encoding = 'utf-8'  # 编码转换
+        soup = BeautifulSoup(response.text, 'lxml')
+        table = soup.find_all('tbody')[1].tbody.tbody.table
+        if attr:
+            trs = table.find_all('tr', attrs={'class': attr})
+        else:
+            trs = table.find_all('tr')
+        return trs
---
+++ src/apply/爬虫/test_calendar.py
@@ -0,0 +1,60 @@
+import datetime
+from unittest import TestCase
+
+from sqlalchemy import Column, Date
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import declarative_base
+
+from conf.config import localhost_oa_engine, localhost_oa_session
+
+Base = declarative_base()
+
+
+class Calendar(Base):
+    __tablename__ = 'calendar'
+    date = Column(Date, primary_key=True, comment="日期")
+    year = Column(INTEGER, comment="年")
+    month = Column(INTEGER, comment="月")
+    day = Column(INTEGER, comment="日")
+    week = Column(INTEGER, comment="周")
+    week_year = Column(INTEGER, comment="一年第几周")
+    quarter = Column(INTEGER, comment="季度")
+    half_year = Column(INTEGER, comment="半年")
+
+
+class Test(TestCase):
+    def test_pre(self):
+        ...
+        Base.metadata.drop_all(localhost_oa_engine)
+        Base.metadata.create_all(localhost_oa_engine)
+
+    def test_main(self):
+        self.test_pre()
+
+        year = 100
+        start_date = datetime.date(2020, 1, 1)
+        end_date = datetime.date(2020 + year, 1, 1)
+        dlt = end_date - start_date
+
+        cur_date = start_date
+        next_day = datetime.timedelta(days=1)
+        day_list = []
+        for i in range(dlt.days):
+            week = cur_date.weekday() + 1
+            week_year = cur_date.isocalendar()[1]
+            quarter = cur_date.month // 3 + 1
+            half_year = quarter // 2 + 1
+            vo = Calendar(date=cur_date, year=cur_date.year, month=cur_date.month, day=cur_date.day,
+                          week=week, week_year=week_year, quarter=quarter, half_year=half_year)
+            day_list.append(vo)
+            if len(day_list) > 1000:
+                self.save_all(day_list)
+            cur_date += next_day
+        if len(day_list) > 0:
+            self.save_all(day_list)
+
+    @staticmethod
+    def save_all(day_list):
+        localhost_oa_session.add_all(day_list)
+        localhost_oa_session.commit()
+        day_list.clear()
