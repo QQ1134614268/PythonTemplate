d1966021ee2ec04ddbb46b7b71e63482a7b8a369
wg <1134614268@qq.com>
2022-09-06 23:48:07 +0800

    u

--- src/main.py
+++ src/main.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     run_3306(2233)
     exec_profile()
 
-# D:\workspace\pythonSpace\PythonTemplate\venv\Scripts\pyinstaller -p D:\workspace\pythonSpace\PythonTemplate\src -D src\main.py
-# D:\workspace\pythonSpace\PythonTemplate\venv\Scripts\pyinstaller -p D:\workspace\pythonSpace\PythonTemplate\src --noconfirm -D main.py
+# D:\workspace\pythonSpace\PythonTemplate\venv\Scripts\pyinstaller -y -F -p D:\workspace\pythonSpace\PythonTemplate\src  D:\workspace\pythonSpace\PythonTemplate\src\main.py
+# D:\workspace\pythonSpace\PythonTemplate\venv\Scripts\pyinstaller -p D:\workspace\pythonSpace\PythonTemplate\src -F src\main.py
+# D:\workspace\pythonSpace\PythonTemplate\venv\Scripts\pyinstaller -y -p D:\workspace\pythonSpace\PythonTemplate\src -D main.py
