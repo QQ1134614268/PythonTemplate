2616b332a56acd587c5a2b7ca4e256698aa289e6
黄冉 <huangran@szcentral.com>
2022-12-19 18:40:06 +0800

    u

--- src/config/db_conf.py
+++ src/config/db_conf.py
@@ -3,20 +3,22 @@
 @Time: 2022/9/22
 @Description:
 """
+from config.conf import DEBUG_MODE
+
+from urllib.parse import quote
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from config.conf import DEBUG_MODE
 
 HOST = "127.0.0.1"
 PORT = "3306"
 USERNAME = 'root'
 PASSWORD = "123456"
 DB = "test"
-localhost_test_url = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}?charset=utf8mb4'
+localhost_test_url = f'mysql+mysqlconnector://{quote(USERNAME)}:{quote(PASSWORD)}@{HOST}:{PORT}/{DB}?charset=utf8mb4'
 localhost_test_engine = create_engine(localhost_test_url, echo=DEBUG_MODE)
 localhost_test_session = sessionmaker(bind=localhost_test_engine)()
-localhost_oa_url = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/oa?charset=utf8mb4'
+localhost_oa_url = f'mysql+mysqlconnector://{quote(USERNAME)}:{quote(PASSWORD)}@{HOST}:{PORT}/coastal-archive?charset=utf8mb4'
 localhost_oa_engine = create_engine(localhost_oa_url, echo=DEBUG_MODE)
 localhost_oa_session = sessionmaker(bind=localhost_oa_engine)()
 ORACLE_NAME = "system"
@@ -24,7 +26,7 @@ ORACLE_PASSWORD = "oracle"
 ORACLE_IP = "127.0.0.1"
 ORACLE_PORT = "15210"
 ORACLE_DB = "helowin"
-oracle_url = f"oracle+cx_oracle://{ORACLE_NAME}:{ORACLE_PASSWORD}@{ORACLE_IP}:{ORACLE_PORT}/?service_name={ORACLE_DB}"
+oracle_url = f"oracle+cx_oracle://{quote(ORACLE_NAME)}:{quote(ORACLE_PASSWORD)}@{ORACLE_IP}:{ORACLE_PORT}/?service_name={ORACLE_DB}"
 oracle_engine = create_engine(oracle_url)
 time_zone = "%2B10:00"
-time_zone_url = f'mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/test?time_zone={time_zone}'
+time_zone_url = f'mysql+mysqlconnector://{quote(USERNAME)}:{quote(PASSWORD)}@{HOST}:{PORT}/test?time_zone={time_zone}'
