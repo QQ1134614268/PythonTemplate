c075423f8a3fa18f3c4e8ef24ce6557d96e0f01b
黄冉 <huangran@szcentral.com>
2023-04-27 10:33:50 +0800

    u

--- src/apply/test/test_mysql/mysql_shell.sql
+++ src/apply/test/test_mysql/mysql_shell.sql
@@ -1,3 +1,4 @@
+# mysql
 --
 select * from INFORMATION_SCHEMA.TABLES;
 select * from INFORMATION_SCHEMA.COLUMNS;
@@ -7,23 +8,25 @@ show full fields from CBondDescription;
 desc CBondDescription;
 show create table CBondDescription;
 
+SHOW VARIABLES LIKE 'sql_mode';
+
 --
 alter table user engine=InnoDB; -- 命令来重建表。
 OPTIMIZE table user;
 
---
+-- mysql 数据问题定位
+show full processlist;
+
+SELECT * from information_schema.processlist;
+
 select * from information_schema.innodb_trx;
 
 select * from information_schema.innodb_locks;
 
 select * from information_schema.innodb_lock_waits;
 
-SHOW VARIABLES LIKE 'sql_mode';
-
 SHOW TABLE STATUS LIKE 'securities_score';
 
 show open tables;
-
-show full processlist;
 
 KILL 27;
