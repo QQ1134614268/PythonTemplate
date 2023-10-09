a0290a2d80b83d6c2cfbd51e53937654bfc51e60
黄冉 <huangran@szcentral.com>
2023-10-09 11:30:20 +0800

    u

--- src/apply/issue/iss_catch_signal.py
+++ src/apply/issue/iss_catch_signal.py
@@ -1,54 +1,50 @@
+# -*- coding:utf-8 -*-
 import signal
 import os
 import time
 
-data = {
-    "end": True
-}
-
 
 def receiveSignal(signalNumber, frame):
+    with open("out.log", "a") as f:
+        f.write(f"Received: {signalNumber}\r\n")
     print('Received:', signalNumber)
-    if signalNumber == signal.SIGKILL:
-        print("end SIGKILL")
-        data["end"] = False
     return
 
 
 if __name__ == '__main__':  # register the signals to be caught
 
-    signal.signal(signal.SIGHUP, receiveSignal)
+    signal.signal(signal.SIGHUP, receiveSignal)  # win 不支持
 
     signal.signal(signal.SIGINT, receiveSignal)
 
-    signal.signal(signal.SIGQUIT, receiveSignal)
+    # signal.signal(signal.SIGQUIT, receiveSignal)  # win 不支持
 
     signal.signal(signal.SIGILL, receiveSignal)
 
-    signal.signal(signal.SIGTRAP, receiveSignal)
+    # signal.signal(signal.SIGTRAP, receiveSignal)  # win 不支持
 
     signal.signal(signal.SIGABRT, receiveSignal)
 
-    signal.signal(signal.SIGBUS, receiveSignal)
+    # signal.signal(signal.SIGBUS, receiveSignal) # win 不支持
 
     signal.signal(signal.SIGFPE, receiveSignal)
 
-    signal.signal(signal.SIGKILL, receiveSignal)
+    # signal.signal(signal.SIGKILL, receiveSignal) # 不能捕捉
 
-    signal.signal(signal.SIGUSR1, receiveSignal)
+    # signal.signal(signal.SIGUSR1, receiveSignal) # win 不支持
 
-    signal.signal(signal.SIGSEGV, receiveSignal)
+    # signal.signal(signal.SIGSEGV, receiveSignal) # win 不支持
 
-    signal.signal(signal.SIGUSR2, receiveSignal)
+    # signal.signal(signal.SIGUSR2, receiveSignal) # win 不支持
 
-    signal.signal(signal.SIGPIPE, receiveSignal)
+    # signal.signal(signal.SIGPIPE, receiveSignal) # win 不支持
 
-    signal.signal(signal.SIGALRM, receiveSignal)
+    # signal.signal(signal.SIGALRM, receiveSignal) # win 不支持
 
     signal.signal(signal.SIGTERM, receiveSignal)
 
     print('My PID is:', os.getpid())  # wait in an endless loop for signals
 
-    while data["end"]:
+    while True:
         print('Waiting...')
         time.sleep(3)
