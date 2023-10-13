59d5d96ef7611ee6ba78dfe3b97ab33f27f7cb3a
黄冉 <huangran@szcentral.com>
2023-10-13 10:05:42 +0800

    u

--- src/apply/html/iss_jessibuca/gb28181.js
+++ src/apply/html/iss_jessibuca/gb28181.js
@@ -22,11 +22,19 @@ function getChannel(deviceId, params) {
 }
 
 function get452BanDao() {
+    let now = new Date();
+    let today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+    // 获取地址 历史视频
+    // return get(`http://44.39.19.14:10070/api/playback/start/44030500101325235250/44030500101325535250?startTime=${today}+00:07:00&endTime=${today}+10:00:00`)
+
+    // 直播  测试 视综 002金世纪路 卡顿 webrtc wss://44.39.19.14:10443/rtp/44030500002000520521_44030552001320022002.live.flv
+    return get(`http://44.39.19.14:10070/api/play/start/44030554001320012002/44030554001320012002`)
+
     // 测试卡顿 视频预警, simd wcs
     // return get(`http://44.39.19.14:10070/api/playback/start/44030500101325097172/44030500101325497172?startTime=2023-09-13+17:20:38&endTime=2023-09-13+17:20:59`)
 
     // 测试拖拽,倍速播放, 半岛城邦 4小时
-    return get(`http://44.39.19.14:10070/api/playback/start/44030500101325149136/44030500101325449136?startTime=2023-09-04+00:00:00&endTime=2023-09-04+10:00:00`)
+    // return get(`http://44.39.19.14:10070/api/playback/start/44030500101325149136/44030500101325449136?startTime=2023-09-04+00:00:00&endTime=2023-09-04+10:00:00`)
     // return get(`http://44.39.19.14:10070/api/playback/start/44030500101325107080/44030500101325407080?startTime=2023-09-13+23:59:59&endTime=2023-09-14+10:10:10`)
     // return get(`http://44.39.19.14:10070/api/playback/start/44030500101325166153/44030500101325466153?startTime=2023-09-13+23:59:59&endTime=2023-09-14+04:28:26`)
     // return get(`http://44.39.19.14:10070/api/playback/start/44030500101325166166/44030500101325466166?startTime=2023-09-13+23:59:59&endTime=2023-09-14+11:22:17`)
@@ -39,8 +47,8 @@ function gbQuery(deviceId, channelId, params) {
     return get(`https://44.39.19.14:10070/api/gb_record/query/${deviceId}/${channelId}`, params)
 }
 
-function gbStart() {
-    return get(`http://44.39.19.14:10070/api/playback/start/44030500101325149136/44030500101325449136?startTime=2023-09-04+00:00:00&endTime=2023-09-04+10:30:37`)
+function gbStart(deviceId, channelId, params) {
+    return get(`http://44.39.19.14:10070/api/playback/start/${deviceId}/${channelId}`, params)
 }
 
 function gbStop(deviceId, channelId, streamId) {
--- src/apply/issue/iss_catch_signal.py
+++ src/apply/issue/iss_catch_signal.py
@@ -4,45 +4,35 @@ import os
 import time
 
 
-def receiveSignal(signalNumber, frame):
+def receive_signal(signal_number, frame):
     with open("out.log", "a") as f:
-        f.write(f"Received: {signalNumber}\r\n")
-    print('Received:', signalNumber)
+        f.write(f"Received: {signal_number}\r\n")
+    print('Received:', signal_number)
     return
 
 
 if __name__ == '__main__':  # register the signals to be caught
+    # linux strace更好追踪进程接收的信号
+    signalList = [
+        signal.SIGHUP,  # win 不支持
+        signal.SIGINT,
+        signal.SIGQUIT,  # win 不支持
+        signal.SIGILL,
+        signal.SIGTRAP,  # win 不支持
+        signal.SIGABRT,
+        signal.SIGBUS,  # win 不支持
+        signal.SIGFPE,
+        # signal.SIGKILL # 不能捕捉
+        signal.SIGUSR1,  # win 不支持
+        signal.SIGSEGV,  # win 不支持
+        signal.SIGUSR2,  # win 不支持
+        signal.SIGPIPE,  # win 不支持
+        signal.SIGALRM,  # win 不支持
+        signal.SIGTERM,
+    ]
 
-    signal.signal(signal.SIGHUP, receiveSignal)  # win 不支持
-
-    signal.signal(signal.SIGINT, receiveSignal)
-
-    # signal.signal(signal.SIGQUIT, receiveSignal)  # win 不支持
-
-    signal.signal(signal.SIGILL, receiveSignal)
-
-    # signal.signal(signal.SIGTRAP, receiveSignal)  # win 不支持
-
-    signal.signal(signal.SIGABRT, receiveSignal)
-
-    # signal.signal(signal.SIGBUS, receiveSignal) # win 不支持
-
-    signal.signal(signal.SIGFPE, receiveSignal)
-
-    # signal.signal(signal.SIGKILL, receiveSignal) # 不能捕捉
-
-    # signal.signal(signal.SIGUSR1, receiveSignal) # win 不支持
-
-    # signal.signal(signal.SIGSEGV, receiveSignal) # win 不支持
-
-    # signal.signal(signal.SIGUSR2, receiveSignal) # win 不支持
-
-    # signal.signal(signal.SIGPIPE, receiveSignal) # win 不支持
-
-    # signal.signal(signal.SIGALRM, receiveSignal) # win 不支持
-
-    signal.signal(signal.SIGTERM, receiveSignal)
-
+    for sig in signalList:
+        signal.signal(sig, receive_signal)
     print('My PID is:', os.getpid())  # wait in an endless loop for signals
 
     while True:
