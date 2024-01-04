485d01a5bd9695f27db1c2120d234708c7386da3
wg <1134614268@qq.com>
2024-01-04 16:06:25 +0800

    u

--- src/apply/test/test_exec_js/test_exec_js.py
+++ src/apply/test/test_exec_js/test_exec_js.py
@@ -1,12 +1,36 @@
+from unittest import TestCase
+
 import execjs
+from execjs import runtime_names
+
+'''r
+JScript nodejs Phantomjs, js环境不一样,识别语法不一样
+默认JScript, 系统环境变量path有nodejs时, 使用nodejs; idea开启时重新加载 path变量? 
+'''
 
 
-def get_menu_list():
-    with open('test_input.js', encoding='utf-8') as f:
-        doc_js = execjs.compile(f.read())
-    # python.exe -X utf8 s2.py
-    # 不能有 export
+class TestExecjs(TestCase):
 
-    # 调用函数
-    # res = doc_js.call('createGuid')
-    return doc_js.eval('menu_list')
+    def test0(self):
+        # os.environ["EXECJS_RUNTIME"] = "Node"
+        node = execjs.get(runtime_names.Node)
+        with open('test_input.js', encoding='utf-8') as f:
+            txt = f.read()
+        ctx = node.compile(txt)
+        print(ctx._runtime.name)
+        data = ctx.eval('menu_list')
+        print(data)
+        data = ctx.call('add', 1, 2)
+        print(data)
+        data = ctx.eval('add(1,2)')
+        print(data)
+
+    def test1(self):
+        default = execjs.get()
+        print(default.eval("1 + 2"))
+
+        jscript = execjs.get(runtime_names.JScript)
+        print(jscript.eval("1 + 2"))
+
+        node = execjs.get(runtime_names.Node)
+        print(node.eval("1 + 2"))
--- src/apply/test/test_exec_js/test_input.js
+++ src/apply/test/test_exec_js/test_input.js
@@ -1,4 +1,7 @@
 let menu_list = [{
-    id: 1,
-    name: "name1",
-}]
+    id: 1, name: "name1",
+}];
+
+function add(num1, num2) {
+    return num1 + num2;
+}
