38880342e03613c1397c81aedaf97287dd53a293
wg <1134614268@qq.com>
2024-06-30 13:30:03 +0800

    u

--- src/apply/test/flask_websocket/read.md
+++ src/apply/test/flask_websocket/read.md
@@ -1,13 +1,14 @@
-[//]: # (todo websocket)
-//     使用原生 WebSocket API 与 Flask 应用建立连接稍微复杂一些，因为 Flask 本身并不直接支持 WebSocket。
-//     但是，你可以使用 Flask-SocketIO 扩展，它基于 Flask 并添加了 WebSocket 和事件驱动通信的功能。
 ```
-WebSocket:
-    Python: # https://blog.csdn.net/xietansheng/article/details/115558069
-    Flask: 
-        socketIo: shixian
-        eventlet
-        gevent gevent-websocket
-    Web:
-    Js: socketIo.js
+Python: # https://blog.csdn.net/xietansheng/article/details/115558069
+websocket: flask_socketio, Flask 本身并不直接支持 WebSocket,使用第三方库
+异步服务器: eventlet | gevent; 避免阻塞,可以支持更多链接
 ```    
+```
+场景:
+    A -> 发送给B, 发送给C; 发送给A,B;
+    群组消息
+    系统推送给所有用户
+
+    post发布消息, 触发推送消息, 推送一个人,推送全部
+    页面, 连接,发送,token, 回显, 前后分离
+```
--- src/apply/test/flask_websocket/test_flask_websocket.py
+++ src/apply/test/flask_websocket/test_flask_websocket.py
@@ -3,57 +3,62 @@
 @Time: 2022/3/4
 @Description:
 
-# todo 重要 (java 也需要), flask websocket
-场景:
-    A -> 发送给B, 发送给C; 发送给A,B;
-    群组消息
-    系统推送给所有用户
-
-    post发布消息, 触发推送消息, 推送一个人,推送全部
-    页面, 连接,发送,token, 回显, 前后分离
 """
-import os
+from flask import Flask, send_file
+from flask_socketio import SocketIO, emit, send
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from flask_socketio import send
-
-app = Flask(__name__, template_folder=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'template'))
-# socketio = SocketIO(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-# 安装完成后，我们可以在 Flask 应用中引入 eventlet 并使用其提供的 WebSocket 服务器来处理 WebSocket 连接。
+app = Flask(__name__, template_folder='./template')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')  # async_mode='eventlet' 或者 'gevent'
 
 
 @app.route('/')
 def index():
-    return render_template('index_socketio.html')
+    # return render_template('index_socketio.html')
+    return send_file('./template/socketio.vue.html')
+
+
+@socketio.on('connect')
+def test_connect():
+    print('connect')
+    emit('connectRes', {'path': '/connect', 'code': 1, 'data': 'connected'})
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
+@socketio.on('message')
+def test_message(msg: str):
+    print('messageRes: ' + msg)
+    emit('messageRes', {'path': '/message', 'code': 1, 'data': msg})
+
+
+@socketio.on('json')
+def test_json(json):
+    print('json: ' + str(json))
+    emit('jsonRes', {'path': '/json', 'code': 1, 'data': json})
 
 
 @socketio.on('hello')
 def test_connect(msg):
     print('hello', msg)
-    emit('server_response', {'data': 'hello client'})
-
-
-@socketio.on('connect2')
-def test_connect():
-    print('test_connect')
-    emit('server_response', {'data': 'connected'})
+    emit('helloRes', {'path': '/hello', 'code': 1, 'data': 'hello client'})
 
 
 @socketio.on('add')
 def add(a, b):
-    print(a + b)
-    emit('server_response', {'path': '/add', 'data': 'connected'})
+    print('add ', a, b)
+    emit('addRes', {'path': '/add', 'code': 1, 'data': a + b})
 
 
-@socketio.on('json')
-def handle_json(json):
-    print('json: ' + str(json))
-    send(json, json=True)
-    emit('json', json)
+@socketio.on('send')
+def test_websocket_send(json):
+    print('send: ' + str(json))
+    # , room=request.sid, broadcast=True
+    send({'path': '/send', 'desc': 'json', 'code': 1, 'data': json}, json=True)
+    send({'path': '/send', 'desc': 'message', 'code': 1, 'data': json}, json=False)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
