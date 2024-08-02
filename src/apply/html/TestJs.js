b1ef72969f66a7603793ae954b6a4ca520a3a960
wg <1134614268@qq.com>
2024-08-02 11:01:56 +0800

    u

--- src/apply/html/TestJs.js
+++ src/apply/html/TestJs.js
@@ -2,29 +2,22 @@ test()
 
 function test() {
     // 测试lambda
-    testBianli(["a", "b"])
+    testBianli(["aa", 'bb'])
 
-    const arr = [{"sql": "ff"}, {"sql": "ff"}, {"sql": "ff"}];
-    // 测试lambda
-    console.log(filterRepeat(arr));
-    console.log(Array.from(new Set(arr)));
-
-    testSetTimeout()
-    testInterval()
+    console.log('自定义去重', filterRepeat([{"a": "1"}, {"a": "1"}, {"a": "2"}]));
 
     const obj1 = {a: 1};
     const obj2 = {a: 1};
-    const obj3 = obj1;
+    console.log('对象相等: ', Object.is(obj1, obj2), obj1 === obj2);
+    console.log('自定义equals: ', equals(obj1, obj2));
 
-    console.log(Object.is(obj1, obj2)); // false
-    console.log(Object.is(obj1, obj3)); // true
-    console.log(obj1 === obj2); // false
-    console.log(obj1 === obj3); // true
+    testSetTimeout()
 
-    console.log(equals(obj1, obj2)); // true
+    testInterval()
 }
 
 function equals(obj1, obj2) {
+    // 考虑null, keys方法不存在场景
     const obj1Keys = Object.keys(obj1);
     const obj2Keys = Object.keys(obj2);
 
@@ -33,7 +26,7 @@ function equals(obj1, obj2) {
     }
 
     for (let key of obj1Keys) {
-        if (obj1[key] === obj2[key]) {
+        if (obj1[key] !== obj2[key]) {
             return false;
         }
     }
@@ -43,17 +36,6 @@ function equals(obj1, obj2) {
 
 function filterRepeat(arr) {
     // 数组去重 -- 对象数组去重
-
-    // 1.利用Array.from(new Set(arr))去重：
-    // 2.利用includes去重
-    // 3.利用map去重
-    // 4.利用indexOf去重
-    // 5.利用单层for循环去重
-    // 6.利用双层for循环去重
-    // 7.利用递归去重
-    // 8.利用Array.filter和map对象数组去重 （性能较高）
-    // 9.利用Array.filter和Array.includes 对象数组去重
-
     let newArr = []
     for (let a of arr) {
         let flag = false
@@ -69,37 +51,45 @@ function filterRepeat(arr) {
     return newArr;
 }
 
-// testJs 测试js
+// testJs 遍历数据
 function testBianli(arr) {
-    // 测试 遍历js
+    // 测试 遍历数据 Lambda mapReduce
 
     // 1. for遍历
+    for (let i = 0; i < arr.length; i++) {
+        console.log('for-i: ', arr[i])
+    }
     // 2. for in
+    for (const i in arr) {
+        console.log('for-in: ', arr[i])
+    }
     // 3. for of
+    for (const item of arr) {
+        console.log('for-of: ', item)
+    }
     // 4. foreach
     arr.forEach((item, index, arr) => {
-        console.log(item, index)
+        console.log('forEach: ', item, index)
     })
     // 5. map
     arr.map((item, index, arr) => {
-        console.log(item, index)
+        console.log('map: ', item, index)
     })
     // 6. filter
     arr.filter((item, index, arr) => {
-        console.log(item, index)
+        console.log('filter: ', item, index)
     })
     // 7. some
     arr.some((item, index, arr) => {
-        console.log(item, index)
+        console.log('some: ', item, index)
     })
     // 8. every
     arr.every((item, index, arr) => {
-        console.log(item, index)
+        console.log('every: ', item, index)
     })
-
     // 9. reduce
     arr.reduce((total, item, index, arr) => {
-        console.log(total, item, index)
+        console.log('reduce: ', total, item, index)
     })
 }
 
